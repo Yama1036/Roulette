7ce6110174b94a3c8d691ecdfdfa6b7f9d37e608
masaharu-mitiru <masaharu-mitiru@192.168.2.43>
2020-01-27 23:08:47 -0400

    Finish!!

--- src/FrmR.java
+++ src/FrmR.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Yama
+ * Jan 23 2020
+ * Let's play some Roulette!!
  */
 
 import java.util.ArrayList;
